49f65971cf607013eec0072becb81fee9921ed17
Navita Ramasar <navitaramasar14@gmail.com>
2023-08-08 15:20:02 -0400

    Objects lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+const employee = {
+        name : "Navita",
+        streetAdress : "123 Main Street"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updateEmployee = {...employee}
+    updateEmployee[key] = value
+    return updateEmployee
+}
+// updateEmployeeWithKeyAndValue(employee,"name","Jacob")
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const copyEmployee = {...employee}
+    delete copyEmployee[key] 
+    return copyEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key) {
+    delete employee[key] 
+    return employee
+}
